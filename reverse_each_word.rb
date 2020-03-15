f9e9c15050b9514ba2984f8de136136eacce6dc2
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:56:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  my_arr = string.split
+  new_arr = []
+  my_arr.each do |word|
+    new_arr << word.reverse 
+  end
+  new_arr.join(" ")
+end
+
+def reverse_each_word(string)
+  my_arr = string.split 
+  new_arr = []
+  my_arr.collect do |word|
+    new_arr << word.reverse 
+  end
+  new_arr.join(" ")
+end
